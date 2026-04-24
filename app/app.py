58a6a50747d0a782a3ec821c49306bbd785d135b
Thalys <thalys_santos@hotmail.com>
2026-04-23 22:46:00 -0300

    Corrigindo caminho dos imports da vercel

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
 # pip install beanie
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI
 from core.config import settings
 from beanie import init_beanie
